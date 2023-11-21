99b8eed56703a3ccde5c36d6a2fbb99381c98c58
Yaroslav Chelombitko <les4sixstm@gmail.com>
2023-11-21 20:59:44 +0200

    Update ocr_model.py

--- text_recognition/ocr_model.py
+++ text_recognition/ocr_model.py
@@ -12,17 +12,9 @@ class OCRModel:
         :return: The recognized text from the image.
         """
         img_path = str(image)
-        # img_path = "/Users/aragonerua/Documents/GitHub/text-recognition-test-task/data/public_data/1.png"
-        # ocr = PaddleOCR(use_angle_cls=True)
-        # result = ocr.ocr(img_path)
         reader = easyocr.Reader(['en', 'ch_sim'])
         text = reader.readtext(img_path)
         result_string = ""
         for message in text:
             result_string += message[1] + "\n"
         return result_string
-        # return text[0][1]
-
-
-# model = OCRModel()
-# print(model.recognize_text(Path("/Users/aragonerua/Documents/GitHub/text-recognition-test-task/data/public_data/5.png")))
