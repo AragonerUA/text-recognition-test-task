8e264c2fd42aca4d58c02f47241206810ad09e2a
Yaroslav Chelombitko <les4sixstm@gmail.com>
2023-11-21 20:50:51 +0200

    First version

--- text_recognition/ocr_model.py
+++ text_recognition/ocr_model.py
@@ -1,4 +1,6 @@
 from pathlib import Path
+from paddleocr import PaddleOCR
+import easyocr
 
 
 class OCRModel:
@@ -9,4 +11,18 @@ class OCRModel:
         :param image: The path to the image file.
         :return: The recognized text from the image.
         """
-        return 'https://www.youtube.com/watch?v=dQw4w9WgXcQ'
+        img_path = str(image)
+        # img_path = "/Users/aragonerua/Documents/GitHub/text-recognition-test-task/data/public_data/1.png"
+        # ocr = PaddleOCR(use_angle_cls=True)
+        # result = ocr.ocr(img_path)
+        reader = easyocr.Reader(['en', 'ch_sim'])
+        text = reader.readtext(img_path)
+        result_string = ""
+        for message in text:
+            result_string += message[1] + "\n"
+        return result_string
+        # return text[0][1]
+
+
+# model = OCRModel()
+# print(model.recognize_text(Path("/Users/aragonerua/Documents/GitHub/text-recognition-test-task/data/public_data/5.png")))
